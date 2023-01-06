816bac5ef4ad165331e298bf350f2627f4d45e80
Yuvakkrishnan <yuvakkrishnan@gmail.com>
2023-01-06 23:49:32 +0530

    Fourth Commit Error in SQL

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,5 +13,5 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }

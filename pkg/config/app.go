08b4d650cee678165aa0265334b1beab0c5caf1a
Yuvakkrishnan <yuvakkrishnan@gmail.com>
2023-01-06 15:52:10 +0530

    Third Commit all file are completed

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "yuvak:Hello@12/simplerest?")
+	d, err := gorm.Open("mysql", "yuvak:Hello@12/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
